9648de2d4d886f4c018a5406357ba0c0fb06b69f
민준희 <noonserly@naver.com>
2025-01-28 23:20:20 +0900

    두번째 날

--- README.md
+++ README.md
@@ -6,5 +6,7 @@ dart는 web환경인 dartpad가 존재하기는 하지만 vscode에서도 사용
 
 ※참고 자료※
 https://www.youtube.com/watch?v=3x1QAwDUaWo
+
 https://greeentea.tistory.com/entry/VSCode%EB%A1%9C-Dart-%EC%8B%9C%EC%9E%91%ED%95%98%EA%B8%B0
+
 https://devshin93.tistory.com/142
--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 void main(){
-  //문자열이니 string으로 자동 전환
+  
   var name = '코드팩토리';
 
   print(name);  //자동 개행인듯
@@ -12,7 +12,7 @@ void main(){
 
   print(name);
 
-  //정수 타입
+  //정수
   int number1 = 10;
   int number2 = 15;
   int number3 = -20;
@@ -22,5 +22,68 @@ void main(){
   print(number3);
 
   print(number1+number2);
+  print(number1-number2);
+  print(number1/number2);
+  print(number1*number2);
 
+  //실수
+  double num1 = 2.5;
+  double num2 = 0.5;
+
+  print(num1+num2);
+  print(num1-num2);
+  print(num1/num2);
+  print(num1*num2);
+
+  //Boolean
+  bool isTrue = true;
+  bool isFalse = false;
+
+  print(isTrue);
+  print(isFalse);
+
+  //String
+  String pro='다트실습중';
+  String pro2='플러터';
+
+  print(pro);
+  print(pro2);
+
+  print(pro+pro2);
+  print(pro+' '+pro2);
+
+  // var String
+  var name3 = '블랙핑크';
+  var number = 20;
+
+  print(name3.runtimeType); //runtumeType을 통해서 var의 현재 타입 알기 가능
+
+  //Map<String, Map<int, List<double>>> testType = {};  //이런 타입 선언은 복잡하니
+  //var testType2 = {}; //대체할 때, var을 씀
+
+  print('${name} ${name2}');  // %사용하는 선언 방식인듯 
+  print('$name $name2');  // 같은 결과
+
+  //dynamic
+  dynamic na = '코드팩토리';
+
+  print(na);
+
+  dynamic n = 1;
+
+  print(n);
+
+  var na2 = '블랙핑크';
+
+  print(na2);
+
+  print(na.runtimeType);
+  print(na2.runtimeType);
+
+  na = 2;
+
+  // na2 = 5; 이건 오류 발생(이유: var 타입은 처음 선언할 때 타입으로 고정이 되기 때문 반면 dynamic은 전환도 가능)
+
+  //nullable - null이 될 수 있다.
+  //non-nullable - null이 될 수 없다.
 }

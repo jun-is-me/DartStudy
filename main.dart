9feabde8b2b34199fb47bf42a54fc1e787c29d74
민준희 <noonserly@naver.com>
2025-01-28 16:25:15 +0900

    두번째 날 중간

--- main.dart
+++ main.dart
@@ -1,3 +1,26 @@
 void main(){
+  //문자열이니 string으로 자동 전환
   var name = '코드팩토리';
+
+  print(name);  //자동 개행인듯
+
+  var name2 = '레드벨벳';
+
+  print(name2);
+
+  name = '플러터 프로그래밍';
+
+  print(name);
+
+  //정수 타입
+  int number1 = 10;
+  int number2 = 15;
+  int number3 = -20;
+
+  print(number1);
+  print(number2);
+  print(number3);
+
+  print(number1+number2);
+
 }

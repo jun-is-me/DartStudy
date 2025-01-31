4860373761cdb2708a26ae79e9208c368b4e8da5
민준희 <noonserly@naver.com>
2025-02-01 04:01:13 +0900

    네번째 날

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ dart는 web환경인 dartpad가 존재하기는 하지만 vscode에서도 사용
 그래서 환경설정을 하고서 dart를 학습할 예정
 (강의는 dartpad를 사용하나 자신의 편의를 위해서 세팅)
 
+배우는 이유: 크로스 플랫폼에 대한 단순한 흥미가 생겨서 flutter을 학습하기 위해서
+
+강의: https://www.inflearn.com/course/dart-%EC%96%B8%EC%96%B4-%EC%9E%85%EB%AC%B8/dashboard
+
 ※참고 자료※
 https://www.youtube.com/watch?v=3x1QAwDUaWo
 
--- main.dart
+++ main.dart
@@ -90,13 +90,127 @@ void main(){
 
   String? a = '코드팩토리';
 
-  a = null;
+  a = null; //그냥 String하면 null을 넣을 수 없음
 
   print(a);
 
   String b = '블랙핑크';
 
-  print(b!);
+  print(b!);  //!를 붙이면 b는 절대로 null이 아니라는 의미
 
+  //final, const
+  final String c = '코드팩토리';
 
+  print(c);
+
+  // c = '블랙핑크'; (final로 선언 후에는 변경 불가)
+
+  const String d = '블랙핑크';  //final, const 둘 다 타입 선언을 하지 않아도 var 기능까지 해준다.
+
+  print(d);
+
+  // d = '코드팩토리' (const도 선언 후에는 변경 불가)
+
+  //final과 const의 차이를 알기 전에 day, time 클래스를 알아야됨
+
+  final DateTime now = DateTime.now();
+
+  print(now);
+
+  // const DateTime now2 = DateTime.now(); (final과 달리 오류)
+
+  /* 이유: const는 빌드 타임값을 알아야 하지만 DateTime.now()는 빌드 하는 그 순간의 시간값을 가져오는데
+  이것을 미리 알 수는 없으니 사용할 수 없다.
+  
+  반면에 final은 빌드 타임값을 알 필요가 없기에 사용할 수 있는 것이다. 그리고 이것이 둘의 차이가 된다.*/
+
+  //오퍼레이터
+
+  double e = 4.0;
+  print(e % 2);
+  print(e % 3);
+
+  e++;
+
+  print(e);
+
+  e--;
+
+  print(e);
+
+  e += 1;
+  
+  print(e);
+
+  e -= 1;
+
+  print(e);
+
+  e *= 2;
+
+  print(e);
+
+  e /= 2;
+
+  print(e);
+
+  //null 조건 오퍼레이터
+
+  double? f = 4.0;
+
+  f ??= 3.0;
+
+  print(f);
+
+  f = null;
+
+  f ??= 3.0;  //f가 만일 null이면 3.0으로 바꾸란 의미 그러니 위에서는 바뀌지 않는 것
+
+  print(f);
+
+  //비교 오퍼레이터
+
+  int g = 1;
+  int h = 2;
+
+  print(g>h);
+  print(g<h);
+  print(g>=h);
+  print(g<=h);
+  print(g==h);
+  print(g!=h);
+
+  //타입 비교 오퍼레이터
+
+  int i = 1;
+
+  print(i is int);
+  print(i is String);
+
+  print(i is! int);
+  print(i is! String);
+
+  //논리 오퍼레이터
+  bool j = 12 > 10 && 1 > 0;
+
+  print(j);
+
+  bool j2 = 12 > 10 && 0 > 1;
+
+  print(j2);
+
+  bool j3 = 12 > 10 || 1 > 0;
+
+  print(j3);
+
+  bool j4 = 12 > 10 || 0 > 1;
+
+  print(j4);
+
+  //List
+  List <String> blackPink = ['제니', '지수', '로제', '리사'];
+  List <int> numbers = [1,2,3,4,5,6];
+
+  print(blackPink);
+  print(numbers);
 }
